d6b093c4a588c2fc96aa592e01898ecded295ae3
Zoe Poeschl <zpoeschl@gmail.com>
2021-04-17 15:17:10 -0500

    responsive bar chart working

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,40 @@ console.log("app.js loaded");
 // stubs for generating initial charts
 function DrawBargraph(sampleId) {
     console.log(`DrawBargraph(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        //console.log(resultArray);
+        var result = resultArray[0];
+        //console.log(result);
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+    });
 }
 
 function DrawBubblechart(sampleId) {
